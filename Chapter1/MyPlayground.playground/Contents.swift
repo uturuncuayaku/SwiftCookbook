2089d17d9c33a287fcd6a85d7bb98174d2d0639d
uturuncuayaku <57707223+uturuncuayaku@users.noreply.github.com>
2021-04-13 18:04:57 -0500

    Add files via upload

---
+++ Chapter1/MyPlayground.playground/Contents.swift
@@ -0,0 +1,44 @@
+// Author: Andres Trujillo
+// Date: Tues Apr 13 5:27pm
+// Reference Book: Swift Cookbook
+
+import UIKit
+
+var str = "Hello, playground"
+
+let phrase: String = "The quick brown fox jumps over the lazy dog"
+let numOfFoxes: Int = 1
+let numofAnimals: Int = 2
+let avgChars: Float = (3+4+5)/9
+print(avgChars)
+
+/*
+ phrase = "The quick brown ? jumps over the lazy ?" //Doesn't compile
+ */
+var anotherPhrase = phrase
+anotherPhrase = "The quick brown 🦊 jumps over the lazy 🐶"
+print(phrase)
+// output: "The quick brown fox jumps over the lazy dog"
+
+print(anotherPhrase)
+// output: ..same but with emojis
+
+var phraseInfo = "The phrase " +  "has: "
+phraseInfo = phraseInfo + "\(numOfFoxes) fox and \(numofAnimals) animals"
+
+print("Number of characters in phrase: \(phrase.count)")
+
+let multiLineExplanation = """
+  Why is the following phrase often used "The quick brown fox jumps over the lazy dog"
+This phrase contains every letter in the alphabet.
+"""
+
+let phrasesAreEqual = phrase == anotherPhrase
+print(phrasesAreEqual)
+//false
+
+let phraseHas43Characters  = phrase.count == 40+3
+print(phraseHas43Characters)
+//true
+
+
